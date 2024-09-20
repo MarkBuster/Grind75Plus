bf936e6612395970b3a893b9d63479e55e7aa4f4
MarkBuster <146307617+MarkBuster@users.noreply.github.com>
2024-09-20 16:59:06 -0700

    MergeStringsAlternately completed and passing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        MergeStringsAlternately obj = new MergeStringsAlternately();
+        System.out.println(obj.mergeAlternately("abc", "pqr"));
+        System.out.println(obj.mergeAlternately("abc", "pqrstu"));
     }
 }
